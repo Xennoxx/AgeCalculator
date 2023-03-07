25e86445d0584d36445971cc888ad3648ec51417
Xennoxx <nicknifffe@gmail.com>
2023-03-07 23:02:08 +0100

    Projektdateien hinzufügen.

---
+++ AgeCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using AgeCalculator;
+
+class program
+{
+    public void Main(string[] args)
+    {
+        Console.WriteLine("Welcome!");
+        Console.WriteLine("Type in you Birth Date ...");
+        string BirthDate = "";
+        PatternMenu MainMenu = new ();
+        MainMenu.MyAge(BirthDate);
+        MainMenu.PatternHeadLineMethod("Main Menu");
+        MainMenu.PatternTextMethod("Claculate how long you lived", 1);
+        MainMenu.PatternTextMethod("Calculate how long it takes too be 100 Years old", 2);
+        MainMenu.PatternTextMethod("Quit the program?", 3);
+
+    }
+}
